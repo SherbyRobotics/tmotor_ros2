7bfaaeac9f6b61d2cb8df9cd05a10629f6b4d6d4
Alexandre Girard <alx87grd@gmail.com>
2023-05-05 14:26:17 -0400

    added launch file and nodes in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'tmotor_ros2'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "tmotor_ros2 = tmotor_ros2.tmotor_ros2:main",
+            "basic_2dof_controller = tmotor_ros2.basic_2dof_controller:main",
+            "pyro_2dof_controller = tmotor_ros2.pyro_2dof_controller:main"
         ],
     },
 )
